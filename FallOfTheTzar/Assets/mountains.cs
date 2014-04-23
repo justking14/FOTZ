296de6335a36329b364e2478e4deb815dd2418c0
Your Name <wpheloung@gmail.com>
2014-04-23 09:52:47 -0400

    no message

---
+++ FallOfTheTzar/Assets/mountains.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mountains : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		foreach(GameObject g in GameObject.FindGameObjectsWithTag("Generic Building")){
+			Destroy(g);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
